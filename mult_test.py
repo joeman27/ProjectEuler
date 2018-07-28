2549c4dbfbc634b89fc2f941a2b1ddda5be614d7
Joe Mankovecky <jmankovecky21@gmail.com>
2018-07-28 15:59:06 -0400

    Uploaded working solution and test cases

---
+++ mult_test.py
@@ -0,0 +1,46 @@
+#Copyright (C) 2018 joeman27
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import multiples_3_5 as ml
+
+def test_mult_3():
+    assert ml.mult_3(0) == 0
+    assert ml.mult_3(4) == 3
+    assert ml.mult_3(6) == 3
+    assert ml.mult_3(10) == 18
+
+def test_mult_5():
+    assert ml.mult_5(0) == 0
+    assert ml.mult_5(6) == 5
+    assert ml.mult_5(10) == 5
+    assert ml.mult_5(11) == 15
+    
+def test_findsum():
+    assert ml.findsum(0) == 0
+    assert ml.findsum(6) == 8
+    assert ml.findsum(10) == 23
+    assert ml.findsum(100) == 2318
+
+def main(args):
+    
+    if args == "--t" or args == "--v":
+        test_mult_3()
+        test_mult_5()
+        test_findsum()
+        if args == "--v":
+            print("Tests completed successfully")
+
+if __name__ == "__main__":
+    main(None)
---
+++ multiples_3_5.py
@@ -0,0 +1,54 @@
+#Copyright (C) 2018 joeman27
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+def mult_3(max):
+    j = 0
+    j_sum = 0
+    
+    while j < max:
+        if j % 5 != 0:
+            j_sum+=j
+        j += 3
+        
+    return j_sum
+    
+    
+def mult_5(max):
+    k = 0
+    k_sum = 0
+    
+    while k < max:
+        k_sum+=k
+        k+=5
+        
+    return k_sum
+    
+    
+def findsum(max):
+    print(mult_3(max) + mult_5(max))
+    
+    
+if __name__ == "__main__":
+    import sys
+    
+    try:
+        import mult_test
+        mult_test.main(sys.argv[2])
+    except IndexError:
+        pass
+    
+    max = int(sys.argv[1])
+    findsum(max)
